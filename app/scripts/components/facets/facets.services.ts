eeaee26f80548d932b74214d369f765da431b2c6
Terry Lin <Terry.Lin@oicr.on.ca>
2016-04-26 12:53:27 -0400

    fix(file): removed metadata file link
    Also, apply a fix from active-portal.
    
    Closes #2186, #2171

--- app/scripts/components/facets/facets.services.ts
+++ app/scripts/components/facets/facets.services.ts
@@ -274,7 +274,7 @@ module ngApp.components.facets.services {
           name: fieldName,
           title: fieldName,
           collapsed: false,
-          facetType: fieldType === 'long' ? 'range' : fieldName.includes('datetime') ? 'datetime' : 'terms',
+          facetType: fieldType === 'long' ? 'range' : _.includes(fieldName, 'datetime') ? 'datetime' : 'terms',
           removable: true
       });
       this.save();
