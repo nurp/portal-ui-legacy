ba236aba6ea7d7d52e139bbbf010ce542ea9296d
Shane Wilson <shane.wilson@oicr.on.ca>
2016-06-15 11:58:55 -0400

    .

--- karma.entry.js
+++ karma.entry.js
@@ -1,3 +1,3 @@
-const context = require.context('./src/js/', true, /.+\.js$/);
+var context = require.context('./src/js/', true, /.+\.js$/);
 context.keys().forEach(context);
 module.exports = context;
