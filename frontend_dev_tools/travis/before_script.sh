0d3bbe888b0a369b55671cd22b1aacb777f68e08
Shane Wilson <shane.wilson@oicr.on.ca>
2016-07-14 18:31:23 -0400

    udpaet travis script 14

--- frontend_dev_tools/travis/before_script.sh
+++ frontend_dev_tools/travis/before_script.sh
@@ -2,6 +2,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-export CHROME_BIN=chromium-browser
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
+ export CHROME_BIN=/usr/bin/google-chrome
+ export DISPLAY=:99.0
+ sh -e /etc/init.d/xvfb start
+ sudo apt-get update
+ sudo apt-get install -y libappindicator1 fonts-liberation
+ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+ sudo dpkg -i google-chrome*.deb
