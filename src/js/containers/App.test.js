11be044360386eda10df013f825dfd2b9898af90
Shane Wilson <shane.wilson@oicr.on.ca>
2016-07-13 23:55:34 -0400

    fixes tests

--- src/js/containers/App.test.js
+++ src/js/containers/App.test.js
@@ -1,16 +1,16 @@
 import { shallow } from 'enzyme';
-import React from 'react';
+import { h } from 'react-hyperscript-helpers';
 
-import { App } from './App';
+import App from 'containers/App';
 
 describe('<App />', () => {
   it('should exist', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(h(App));
     expect(wrapper.type()).to.equal('div');
   });
 
-  it('should contain top level components', () => {
-    const wrapper = shallow(<App />);
+  it('should be able to pass children', () => {
+    const wrapper = shallow(h(App, { children: [1, 2, 3, 4] }));
     expect(wrapper.children()).to.have.length(4);
   });
 });
