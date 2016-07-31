bec67f3266241fdca044fa52f959a07cd242469e
Alex Wilmer <alex.wilmer@oicr.on.ca>
2016-07-31 15:02:30 -0400

    feat(components): CurrentFilter links

--- src/js/components/CurrentFilters.js
+++ src/js/components/CurrentFilters.js
@@ -2,10 +2,14 @@
 import React, { PropTypes } from 'react'
 import { Link, withRouter } from 'react-router'
 import UndoIcon from 'react-icons/lib/md/undo'
+import Color from 'color'
 
 // Custom
+import { setFilters, setFilter } from 'utils/filters'
 import Button from 'uikit/Button'
 import { Row } from 'uikit/Flex'
+import theme from 'theme'
+import { buttonLike } from 'theme/mixins'
 
 /*----------------------------------------------------------------------------*/
 
@@ -18,10 +22,27 @@ const styles = {
     fontSize: '2rem',
     marginRight: '0.3rem',
   },
+  op: {
+    ...buttonLike,
+    backgroundColor: theme.primary,
+    color: 'white',
+  },
+  field: {
+    backgroundColor: theme.greyScale2,
+    ':hover': {
+      backgroundColor: Color(theme.greyScale2).lighten(0.7).rgbString(),
+    },
+  },
+  value: {
+    backgroundColor: theme.success,
+    ':hover': {
+      backgroundColor: Color(theme.success).lighten(0.7).rgbString(),
+    },
+  },
 }
 
 const CurrentFilters = ({ location }) => {
-  const { filters, ...query } = location.query
+  const { filters, offset, ...query } = location.query
   const currentFilters = filters ? JSON.parse(filters).content : []
 
   return (
@@ -30,20 +51,64 @@ const CurrentFilters = ({ location }) => {
         <Button leftIcon={<UndoIcon />}>Clear</Button>
       </Link>
 
-      {currentFilters.map((filter, i) =>
-        <Row key={filter.content.field} spacing="0.3rem">
-          <Button>{filter.content.field}</Button>
-          {filter.op === 'in' && filter.content.value.length === 1 && <Button>IS</Button>}
-          {filter.op === 'in' && filter.content.value.length > 1 && <Button>IN</Button>}
-          {filter.op !== 'in' && <Button>{filter.op}</Button>}
-          {filter.content.value.length > 1 && <span style={styles.leftParen}>(</span>}
-          {filter.content.value.map(value =>
-            <Button key={value}>{value}</Button>
-          )}
-          {filter.content.value.length > 1 && <span style={styles.rightParen}>)</span>}
-          {i < currentFilters.length - 1 && <Button >AND</Button>}
-        </Row>
-      )}
+      {currentFilters.map((filter, i) => {
+        const nextFilters = setFilters(currentFilters.filter(x =>
+          x.content.field !== filter.content.field
+        ))
+
+        return (
+          <Row key={filter.content.field} spacing="0.3rem">
+            <Link
+              to={{
+                pathname: location.pathname,
+                query: {
+                  ...query,
+                  ...(nextFilters ? {
+                    filters: JSON.stringify(nextFilters),
+                    offset,
+                  } : {}),
+                },
+              }}
+            >
+              <Button style={styles.field}>{filter.content.field}</Button>
+            </Link>
+            {filter.op === 'in' && filter.content.value.length === 1 &&
+              <span style={styles.op}>IS</span>
+            }
+            {filter.op === 'in' && filter.content.value.length > 1 &&
+              <span style={styles.op}>IN</span>
+            }
+            {filter.op !== 'in' && <Button style={styles.op}>{filter.op}</Button>}
+            {filter.content.value.length > 1 && <span style={styles.leftParen}>(</span>}
+            {filter.content.value.map(value => {
+              const nextValue = filter.content.value.filter(x => x !== value)
+
+              const nextFilters = nextValue.length // eslint-disable-line
+                ? setFilter({ value: nextValue, field: filter.content.field })
+                : setFilters(currentFilters.filter(x => x.content.field !== filter.content.field))
+
+              return (
+                <Link
+                  to={{
+                    pathname: location.pathname,
+                    query: {
+                      ...query,
+                      ...(nextFilters ? {
+                        filters: JSON.stringify(nextFilters),
+                        offset,
+                      } : {}),
+                    },
+                  }}
+                >
+                  <Button key={value} style={styles.value}>{value}</Button>
+                </Link>
+              )
+            })}
+            {filter.content.value.length > 1 && <span style={styles.rightParen}>)</span>}
+            {i < currentFilters.length - 1 && <Button >AND</Button>}
+          </Row>
+        )
+      })}
     </Row>
   )
 }
--- src/js/theme/mixins.js
+++ src/js/theme/mixins.js
@@ -7,3 +7,11 @@ export const center = {
 export const zDepth1 = {
   boxShadow: '0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12)',
 }
+
+export const buttonLike = {
+  ...center,
+  padding: '6px 12px',
+  fontSize: '14px',
+  borderRadius: '4px',
+  border: '1px solid transparent',
+}
